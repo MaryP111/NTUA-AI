701ad5d84ec987c14e0aadcebf40decb689c0b8f
Giannis Daras <daras.giannhs@gmail.com>
2018-12-17 11:14:41 +0200

    Refactor code heavily

--- lab1/src/taxis/Graph.java
+++ lab1/src/taxis/Graph.java
@@ -7,167 +7,35 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 
-
-
-public class Graph {
-        private long numberOfNodes;
-        private long numberOfEdges;
-        String line = "";
-        public static final String nodesPATH = "taxis/nodes.csv";
-        public static final String taxisPATH = "taxis/taxis.csv";
-        public  HashMap<Long, ArrayList<Connection>> adj = new HashMap<Long, ArrayList<Connection>>();
-        public static HashMap<List<Double>, Node> hashNodes = new HashMap<List<Double>, Node>();
-        public ArrayList<Node> nodes;
-        String [] temp = new String[3];
-        public ArrayList<Node> allNodes = new ArrayList<Node>();
-        long ai=0;
-        public void createGraph() throws IOException {
-            //FileReader fd = new FileReader(nodesPATH);
-            //BufferedReader bufferedReader = new BufferedReader(fd);
-            //bufferedReader.readLine();// try-catch omitted
-            BufferedReader fd = new BufferedReader(new FileReader(nodesPATH));
-            line=fd.readLine();
-            // CSVParser parser = CSVParser.parse(fd, CSVFormat.RFC4180);
-            // for (CSVRecord line : parser) {
-            //     long key = line.getRecordNumber();
-            //     System.out.println(key);
-            //     double x = Double.parseDouble(line.get(0));
-            //     double y = Double.parseDouble(line.get(1));
-            //     long address = Long.parseLong(line.get(2));
-            while((line=fd.readLine())!=null) {
-                ai=ai+1;
-                //line=fd.readLine();
-                temp=line.split(",");
-                double x=Double.parseDouble(temp[0]);
-                double y = Double.parseDouble(temp[1]);
-                long address=Long.parseLong(temp[2]);
-                List<Double> keyList = new ArrayList<Double>();
-
-                keyList.add(x);
-                keyList.add(y);
-                Node n = hashNodes.get(keyList);
-                if (n == null) {
-                        n = new Node(x, y, ai);
-                        n.addresses.add(address);
-                        hashNodes.put(keyList, n);
-                }
-                else {
-                        if (n.addresses.contains(address) == false) n.addresses.add(address);
-                }
-                allNodes.add(n);
-            }
-            fd.close();
-            System.out.println("Nodes parsing completed succesfully");
-            System.out.println(allNodes.size());
-            nodes = new ArrayList<Node>(hashNodes.values());
-            //fd = new FileReader(nodesPATH);
-            //parser = CSVParser.parse(fd, CSVFormat.RFC4180);
-            for (int i=0; i<allNodes.size()-1; i++) {
-                Node node = allNodes.get(i);
-                Node nextNode = allNodes.get(i+1);
-                ArrayList<Connection> nextNodeConnectionList = adj.get(nextNode.key);
-                ArrayList<Connection> connectionList = adj.get(node.key);
-
-                if (connectionList ==  null) {
-                        connectionList = new ArrayList<Connection>();
-                }
-                if (nextNodeConnectionList == null) {
-                        nextNodeConnectionList = new ArrayList<Connection>();
-                }
-                Boolean flag = false;
-                for (long address1 : node.addresses) {
-                        for (long address2 : nextNode.addresses) {
-                                if (address1 == address2) {
-                                        flag = true;
-                                }
-                        }
-                }
-                if (flag) {
-                        node.numberOfNeighbors++;
-                        nextNode.numberOfNeighbors++;
-                        Connection connection_src = new Connection(node, nextNode);
-                        Connection connection_dst = new Connection(nextNode, node);
-                        connectionList.add(connection_src);
-                        nextNodeConnectionList.add(connection_dst);
-                }
-
-                adj.put(node.key, connectionList);
-                adj.put(nextNode.key, nextNodeConnectionList);
-                }
-//          for (Node node: nodes) System.out.println(node.numberOfNeighbors);
-
-
-        }
-        public void revert() {
-            for (int j=0;j<allNodes.size();j++) {
-                allNodes.get(j).distanceFromStart=Double.POSITIVE_INFINITY;
-                allNodes.get(j).fscore=Double.POSITIVE_INFINITY;
-            }
-        }
-
-
-}
-class Node{
-        public double x;
-        public double y;
-        public long key;
-        public int numberOfNeighbors = 0;
-        public double distanceFromStart;
-        public double fscore;
-        public ArrayList<Long> addresses = new ArrayList<Long>();
-        Node(double x, double y, long key){
-                this.x = x;
-                this.y = y;
-                this.key = key;
-                this.distanceFromStart = Double.POSITIVE_INFINITY;
-                this.fscore = Double.POSITIVE_INFINITY;
-        }
-        @Override
-        public boolean equals(Object o) {
-            if (o == this) { 
-                return true; 
-            } 
-      
-            /* Check if o is an instance of Mapnode or not */
-            if (!(o instanceof Node)) { 
-                return false; 
-            } 
-              
-            // typecast o to Mapnode so that we can compare data members  
-            Node m = (Node) o; 
-              
-            // Compare the data members and return accordingly 
-            return (this.x == m.x && this.y == m.y); 
-             
-        
+class Point{
+	public double x, y;
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) return true;
+        if (!(other instanceof Node)) return false;
+        Node casted_other = (Node) other;
+        return (this.x == casted_other.x && this.y == casted_other.y); 
     }
-
-        public double distance(Node m) {
-            final int R = 6371; // Radius of the earth
-            double latDistance = Math.toRadians(m.x - this.x);
-            double lonDistance = Math.toRadians(m.y - this.y);
-            double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2) 
-                    + Math.cos(Math.toRadians(this.x)) * Math.cos(Math.toRadians(m.x))* Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
-            double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-            double distance = R * c * 1000; // convert to meters
-            return distance;
-    }
-        public Node find_nearest_node (ArrayList <Node> nodes ) {
-        double min_dist, dist;
+    public Node findNearestNode (ArrayList<Node> nodes ) {
+        double minDist, dist;
         Node min;
-        min=nodes.get(0);
-        min_dist = this.distance(min);
-            for (int j=1;j<nodes.size();j++) {
-                dist = this.distance(nodes.get(j));
-                if (dist < min_dist) {
-                    min_dist = dist;
+        min = nodes.get(0);
+        minDist = Distance.computeDistance(this, min);
+            for (int j = 1; j < nodes.size();j++) {
+                dist = Distance.computeDistance(this, nodes.get(j));
+                if (dist < minDist) {
+                    minDist = dist;
                     min = nodes.get(j);
             }
         }
@@ -175,25 +43,228 @@ class Node{
     }
 }
 
+class Node extends Point{
+    public long key;
+    public int numberOfNeighbors = 0;
+    public double distanceFromStart;
+    public double fscore;
+    public ArrayList<Long> addresses = new ArrayList<Long>();
+    Node(double x, double y, long key){
+        this.x = x;
+        this.y = y;
+        this.key = key;
+        this.distanceFromStart = Double.POSITIVE_INFINITY;
+        this.fscore = Double.POSITIVE_INFINITY;
+    }
+}
+
+
+class Taxi extends Point{
+	public long id;
+	public Node nearestNode;
+	Taxi (double x, double y, long id, ArrayList<Node> nodes) {
+		this.x = x;
+		this.y = y;
+		this.id = id;				
+		this.nearestNode = this.findNearestNode(nodes);
+	}
+}
+
+public class Graph {
+        public static final String clientPATH = "data/client.csv";
+        public static final String nodesPATH = "data/nodes.csv";
+        public static final String taxisPATH = "data/taxis.csv";
+        public  HashMap<Long, ArrayList<Connection>> adj = new HashMap<Long, ArrayList<Connection>>();
+        public static HashMap<List<Double>, Node> hashNodes = new HashMap<List<Double>, Node>();
+        /* nodes are the discrete nodes of the graph */
+        public ArrayList<Node> nodes;
+        /* all noNodes are the nodes of the graph as read from the csv file */
+        public ArrayList<Node> allNodes = new ArrayList<Node>();
+    	public ArrayList<Taxi> allTaxis = new ArrayList<Taxi>();
+    	
+    	
+        public void createGraph() throws IOException {
+        	FileReader fd = new FileReader(nodesPATH);
+			CSVParser parser = CSVParser.parse(fd, CSVFormat.RFC4180);
+			for (CSVRecord line : parser) {
+			   long key = line.getRecordNumber();
+			   double x = Double.parseDouble(line.get(0));
+			   double y = Double.parseDouble(line.get(1));
+			   long address = Long.parseLong(line.get(2));
+			   List<Double> keyList = new ArrayList<Double>();
+			   keyList.add(x);
+			   keyList.add(y);
+			   Node n = hashNodes.get(keyList);
+			   if (n == null) {
+		            n = new Node(x, y, key);
+		            n.addresses.add(address);
+		            hashNodes.put(keyList, n);
+			    }
+			    else {
+		            if (n.addresses.contains(address) == false) n.addresses.add(address);
+			    }
+			    allNodes.add(n);
+			}
+            fd.close();
+//            System.out.println("Nodes parsing completed succesfully");
+//            System.out.println(allNodes.size());
+            nodes = new ArrayList<Node>(hashNodes.values());
+            for (int i=0; i<allNodes.size()-1; i++) {
+                Node node = allNodes.get(i);
+                Node nextNode = allNodes.get(i+1);
+                ArrayList<Connection> nextNodeConnectionList = adj.get(nextNode.key);
+                ArrayList<Connection> connectionList = adj.get(node.key);
+
+                if (connectionList ==  null) {
+                    connectionList = new ArrayList<Connection>();
+                }
+                if (nextNodeConnectionList == null) {
+                    nextNodeConnectionList = new ArrayList<Connection>();
+                }
+                Boolean flag = false;
+                for (long address1 : node.addresses) {
+                    for (long address2 : nextNode.addresses) {
+                            if (address1 == address2) flag = true;
+                    }
+                }
+                if (flag) {
+					node.numberOfNeighbors++;
+					nextNode.numberOfNeighbors++;
+					Connection connection_src = new Connection(node, nextNode);
+					Connection connection_dst = new Connection(nextNode, node);
+					connectionList.add(connection_src);
+					nextNodeConnectionList.add(connection_dst);
+                }
+                adj.put(node.key, connectionList);
+                adj.put(nextNode.key, nextNodeConnectionList);
+                }
+        }
+        
+        public void createTaxis() throws IOException{
+        	FileReader fd = new FileReader(nodesPATH);
+            CSVParser parser = CSVParser.parse(fd, CSVFormat.RFC4180);
+             for (CSVRecord line : parser) {
+                 double x = Double.parseDouble(line.get(0));
+                 double y = Double.parseDouble(line.get(1));
+                 long address = Long.parseLong(line.get(2));
+                 allTaxis.add(new Taxi(x, y, address, nodes));
+             }
+        }
+        
+        public void run() throws IOException {
+        	Reader fd = new FileReader(clientPATH);
+            CSVParser parser = CSVParser.parse(fd, CSVFormat.RFC4180);
+            double x;
+            double y;
+            for (CSVRecord line : parser) {
+            	x = Double.parseDouble(line.get(0));
+                y = Double.parseDouble(line.get(1));
+             }
+        }
+}
+
+
 class Connection{
         public Node node;
         public double cost;
         Connection(Node src, Node dst){
                 this.node = dst;
-                this.cost = computeDistance(src, dst);
+                this.cost = Distance.computeDistance(src, dst);
         }
+}
 
-        public double computeDistance(Node src, Node dst) {
-            int R = 6371; // Radius of the earth
-            double latDistance = Math.toRadians(dst.x - src.x);
-            double lonDistance = Math.toRadians(dst.y - src.y);
-            double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2) 
-                    + Math.cos(Math.toRadians(src.x)) * Math.cos(Math.toRadians(dst.x))* Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
-            double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-            double distance = R * c * 1000; // convert to meters
-            return distance;
+
+class AstarSearch {
+	public AstarResult aStarSearch(Graph g1,Node startNode,Node endNode) {
+	        HashMap<Long,ArrayList<Node>> parentsMap = new HashMap<Long,ArrayList<Node>>();
+	        HashSet<Node> visited = new HashSet<Node>();
+	        ArrayList <Node> frontier = new ArrayList<>();
+	        startNode.distanceFromStart = 0;
+	        startNode.fscore = Distance.computeDistance(startNode, endNode);
+	        frontier.clear();
+	        visited.clear();
+	        frontier.add(startNode);
+	        Node current = null;
+	        while (!frontier.isEmpty()) {
+	            current = frontier.get(0);
+	            frontier.remove(0);
+	            if(!visited.contains(current) ) {
+	                visited.add(current);
+	                if (current.key == endNode.key) {
+	                    System.out.println("Found");
+	                    return new AstarResult(current.distanceFromStart,parentsMap);
+	                }
+	                ArrayList<Connection> neighbors = g1.adj.get(current.key);
+	                for (int j=0;j<neighbors.size();j++) {
+	                    Node neighbor=neighbors.get(j).node;
+	                    if (!visited.contains(neighbor) ){  
+	                        double predictedDistance = Distance.computeDistance(neighbor, endNode);
+	                        double totalDistance =current.distanceFromStart +neighbors.get(j).cost+ predictedDistance;
+	                        if(totalDistance < neighbor.fscore ){
+	                            // update n's distance
+	                            neighbor.fscore=totalDistance;
+	                            neighbor.distanceFromStart=current.distanceFromStart+neighbors.get(j).cost;
+	                            // set parent
+	                            if (parentsMap.containsKey(neighbor.key)) {
+	                            ArrayList<Node> parents=parentsMap.get(neighbor.key);
+	                            parents.add(current);
+	                            parentsMap.put(neighbor.key,parents);
+	                            }
+	                            else {
+	                                ArrayList<Node> parents=new ArrayList<Node>();
+	                                parents.add(current);
+	                                parentsMap.put(neighbor.key,parents);
+	                            }
+	                            frontier.add(neighbor);
+	                        }
+	                    }
+	                }
+	            }
+	            Collections.sort(frontier, new Comparator<Node>(){
+	            	public int compare(Node point1, Node point2){
+	            		if(point1.fscore == point2.fscore) return 0;
+	                    return (point1.fscore ) < (point2.fscore)? -1 : 1;
+	                     }
+	                });
+	
+	        }
+	        return null;
+	    }
+}
+
+
+class AstarResult {
+    public double score;
+    public HashMap<Long,ArrayList<Node>> routes;
+    AstarResult(double score, HashMap<Long, ArrayList<Node>> parentsMap){
+        this.score = score;
+        this.routes = new HashMap<Long, ArrayList<Node>>();
+        for (Map.Entry<Long, ArrayList<Node>> entry : parentsMap.entrySet()) {
+            Long key = entry.getKey();
+            ArrayList<Node> path = new ArrayList<Node>(entry.getValue());
+            this.routes.put(key,path);
+//            System.out.println(this.routes.get(key).get(0));
         }
     }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
