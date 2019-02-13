2df2c15dababda9849bd20a2f5aa99b764caf194
Giannis Daras <daras.giannhs@gmail.com>
2019-02-13 19:05:07 +0200

    Fix bug with line directions and move to neighbor(x,y) logic for prolog queries

--- lab2/src/taxis/PrologConverter.java
+++ lab2/src/taxis/PrologConverter.java
@@ -3,6 +3,7 @@ package taxis;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 import org.apache.commons.csv.CSVFormat;
@@ -19,17 +20,91 @@ public class PrologConverter {
 
 
 	public void createPredicates() throws IOException {
+		HashMap<Long, String> lines = new HashMap<Long, String>();
 		FileReader fd = new FileReader(nodesPATH);
 		CSVParser parser = CSVParser.parse(fd, CSVFormat.RFC4180);
 		PrintWriter writer = null;
-		PrintWriter writer2= null;
-		PrintWriter writer3= null;
+		PrintWriter writerLines= null;
+		PrintWriter writerClientDriver= null;
 		File file = new File("./data/input.pl");
 		File file2 = new File("./data/lines.pl");
 		File file3 = new File("./data/client_driver.pl");
 		writer = new PrintWriter(file);
-		writer2 = new PrintWriter(file2);
-		writer3 = new PrintWriter(file3);
+		writerLines = new PrintWriter(file2);
+		writerClientDriver = new PrintWriter(file3);
+		
+		/* read lines */
+		FileReader fd4 = new FileReader(linesPATH);
+		CSVParser parser4 = CSVParser.parse(fd4, CSVFormat.RFC4180);
+		for (CSVRecord line : parser4) {
+			long id = Long.parseLong(line.get(0));
+			String highway = line.get(1);
+			int priority;
+			if (highway.equals(""))
+				priority=0;
+			else if (highway.equals("primary") || highway.equals("primary_link") || highway.equals("motorway_link"))
+				priority=2;
+			else if (highway.equals("secondary") || highway.equals("secondary_link"))
+				priority=1;
+			else if (highway.equals("residential"))
+				priority=-1;
+			else
+				priority=0;
+			String isOneway = line.get(3);
+			int oneway;
+			if (isOneway.equals("yes")) {
+				oneway=1;
+				lines.put(id, "yes");
+			}
+			else if (isOneway.equals("no")) {
+				oneway=0;
+				lines.put(id, "no");
+			}
+			else{
+				oneway=-1;
+				lines.put(id, "-1");
+			}
+			String isLit = line.get(4);
+			int lit;
+			if (isLit.equals("no")) 
+				lit=0;
+			else
+				lit=1;
+			
+			String lanes =line.get(5);
+			int nolanes=1;
+			if (!lanes.equals("")) {
+				nolanes = Integer.parseInt(lanes);
+				System.out.println(nolanes);
+			}
+			else
+				nolanes=1;
+
+			String hasAccess = line.get(9);
+			int access;
+			if (hasAccess.equals("no")) 
+				access=0;
+			else
+				access=1;
+			
+			String isInclined = line.get(14);
+			int incline;
+			if (isInclined.equals("yes")) 
+				incline=1;
+			else
+				incline=0;
+			
+			String hasTolls = line.get(17);
+			int tolls;
+			if (hasTolls.equals("yes")) 
+				tolls=1;
+			else
+				tolls=0;
+			writerLines.println("line(" + id + "," + priority + "," + oneway +"," + lit + ","  + nolanes + "," + access +  "," + incline + "," + tolls  +").");
+		}
+		
+		
+		/* read nodes */
 		long prevLineId=0;
 		long prevNodeId=0;
 		int i=0;
@@ -38,12 +113,22 @@ public class PrologConverter {
 			double y = Double.parseDouble(line.get(1));
 			long lineId = Long.parseLong(line.get(2));
 			long nodeId = Long.parseLong(line.get(3));
-			if (lineId == prevLineId && i>0)
-				writer.println("neighbors(" + prevNodeId + "," + nodeId + ")." );
 			writer.println("node(" + x + "," + y + ","+ lineId + "," + nodeId + ").");
-			writer.println("belongsTo(" + nodeId + "," + lineId + ").");
-			prevLineId=lineId;
-			prevNodeId=nodeId;
+			if (lines.get(lineId) == "yes") {
+				/* one way line */
+				writer.println("neighbor(" + prevNodeId + "," + nodeId + ").");
+			}
+			else if (lines.get(lineId) == "no") {
+				/* two way line */
+				writer.println("neighbor(" + nodeId + "," + prevNodeId + ").");
+				writer.println("neighbor(" + prevNodeId + "," + nodeId + ").");
+			}
+			else {
+				/* reverse line */
+				writer.println("neighbor(" + nodeId + "," + prevNodeId + ").");
+			}
+			prevLineId = lineId;
+			prevNodeId = nodeId;
 			i=i+1;
 		}
 
@@ -60,11 +145,11 @@ public class PrologConverter {
 //			String language = line.get(5);
 //			int luggage = Integer.parseInt(line.get(6));
 			writer.println("client(" + "23.733912" + "," + "37.975687" + "," + "23.772518" +"," + "38.012301" + "0" + "," + 0 + "," + 3 + "," + "greek" + "," + "1" + ").");
-			writer3.println("client(" + "23.733912" + "," + "37.975687" + "," + "23.772518" +"," + "38.012301" + "0" + "," + 0 + "," + 3 + "," + "greek" + "," + "1" + ").");
+			writerClientDriver.println("client(" + "23.733912" + "," + "37.975687" + "," + "23.772518" +"," + "38.012301" + "0" + "," + 0 + "," + 3 + "," + "greek" + "," + "1" + ").");
 //		}
 
-		//read input for taxis
-
+			
+		/* read input for taxis */
 		FileReader fd3 = new FileReader(taxisPATH);
 		CSVParser parser3 = CSVParser.parse(fd3, CSVFormat.RFC4180);
 		for (CSVRecord line : parser3) {
@@ -101,88 +186,22 @@ public class PrologConverter {
 				maxLuggage=5;
 			else
 				maxLuggage=3;
-			writer.println("taxi(" + x + "," + y + "," + id +"," + free + ","  + capacity + "," +rating + ","+ dLong + "," + maxLuggage +").");
-			writer3.println("taxi(" + x + "," + y + "," + id +"," + free + ","  + capacity + "," +rating + ","+ dLong + "," + maxLuggage +").");
+			writerClientDriver.println("taxi(" + x + "," + y + "," + id +"," + free + ","  + capacity + "," +rating + ","+ dLong + "," + maxLuggage +").");
 			if(languages.equals("greek|english")) {
-				writer3.println("languages("+ id + ",greek).");
-				writer3.println("languages("+ id + ",english).");
+				writerClientDriver.println("languages("+ id + ",greek).");
+				writerClientDriver.println("languages("+ id + ",english).");
 			}
 			else if(languages.equals("greek"))
-				writer3.println("languages("+ id + ",greek).");
+				writerClientDriver.println("languages("+ id + ",greek).");
 			else
-				writer3.println("languages("+ id + ",english).");
+				writerClientDriver.println("languages("+ id + ",english).");
 				
 		}
 
-		//read input for lines
 
 
-		FileReader fd4 = new FileReader(linesPATH);
-		CSVParser parser4 = CSVParser.parse(fd4, CSVFormat.RFC4180);
-		for (CSVRecord line : parser4) {
-			long id = Long.parseLong(line.get(0));
-			String highway = line.get(1);
-			int priority;
-			if (highway.equals(""))
-				priority=0;
-			else if (highway.equals("primary") || highway.equals("primary_link") || highway.equals("motorway_link"))
-				priority=2;
-			else if (highway.equals("secondary") || highway.equals("secondary_link"))
-				priority=1;
-			else if (highway.equals("residential"))
-				priority=-1;
-			else
-				priority=0;
-			String isOneway = line.get(3);
-			int oneway;
-			if (isOneway.equals("yes")) 
-				oneway=1;
-			else
-				oneway=0;
-			String isLit = line.get(4);
-			int lit;
-			if (isLit.equals("no")) 
-				lit=0;
-			else
-				lit=1;
-			
-			String lanes =line.get(5);
-			int nolanes=1;
-			//System.out.println(lanes);
-			if (!lanes.equals("")) {
-				nolanes = Integer.parseInt(lanes);
-				System.out.println(nolanes);
-			}
-			else
-				nolanes=1;
 
-			String hasAccess = line.get(9);
-			int access;
-			if (hasAccess.equals("no")) 
-				access=0;
-			else
-				access=1;
-			
-			String isInclined = line.get(14);
-			int incline;
-			if (isInclined.equals("yes")) 
-				incline=1;
-			else
-				incline=0;
-			
-			String hasTolls = line.get(17);
-			int tolls;
-			if (hasTolls.equals("yes")) 
-				tolls=1;
-			else
-				tolls=0;
-			
-			writer.println("line(" + id + "," + priority + "," + oneway +"," + lit + ","  + nolanes + "," + access +  "," + incline + "," + tolls  +").");
-			writer2.println("line(" + id + "," + priority + "," + oneway +"," + lit + ","  + nolanes + "," + access +  "," + incline + "," + tolls  +").");
-
-			}
-
-		//read traffic data
+		// read traffic data
 		FileReader fd5 = new FileReader(trafficPATH);
 		CSVParser parser5 = CSVParser.parse(fd5, CSVFormat.RFC4180);
 		int j=1;
@@ -199,7 +218,7 @@ public class PrologConverter {
 			}
 		}
 		writer.close();
-		writer2.close();
-		writer3.close();
+		writerLines.close();
+		writerClientDriver.close();
 	}
 }
